c19510d40857135f39a1add82066f23a9340e809
Yasuhiro Usutani <usutani@tgl.co.jp>
2019-02-24 13:34:52 +0900

    total_count_for(11)

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,11 @@
 class BonusDrink
   def self.total_count_for(amount)
-    amount
+    self.calc_bottles(amount, amount)
+  end
+
+  def self.calc_bottles(amount, empty_bottles)
+    return amount if empty_bottles < 3
+    bonus, amari = empty_bottles.divmod(3)
+    calc_bottles(amount + bonus, bonus + amari)
   end
 end
